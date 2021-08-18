450f8acc45dbae074b798ed93f79030f2d64b5c5
Courtney Wilson <cmwilson132@gmail.com>
2021-08-18 13:28:49 -0500

    commit tree c20bc438844b6b529c76085ecbec28e87a5cea7d

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+# require_relative './user'
+
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
